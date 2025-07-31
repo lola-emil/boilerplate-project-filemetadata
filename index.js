25cc2239f2119655ae751f35e309f32628d016c5
Rens Saladaga <staleexam19@gmail.com>
2025-07-31 08:11:29 +0800

    solution

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
-var cors = require('cors');
+const cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+const upload = multer({ dest: 'uploads/' }); // stores files in 'uploads/'
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) return res.status(400).json({ error: 'No file uploaded' });
 
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
